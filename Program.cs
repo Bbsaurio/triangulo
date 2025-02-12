e1da7359015bd430055f80ce43c28e056c1f8b24
sosag <sosag@GAEL>
2025-02-12 16:54:47 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Inserte la base");
+            int b= int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Inserte la altura");
+            int h= int.Parse(Console.ReadLine());
+            int A = (b * h) / 2;
+            System.Console.WriteLine("El resultado es"+A);
+            Console.ReadKey();
+
+        }
+    }
+}
